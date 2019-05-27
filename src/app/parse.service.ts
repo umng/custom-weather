1954b55d1df1cad98633bb5f9993e7e242e9bf22
Umang Patel <umang85patel@gmail.com>
2019-05-27 10:08:02 +0530

    backend migrated to Back4App
    keys and server url updated.

--- src/app/parse.service.ts
+++ src/app/parse.service.ts
@@ -9,8 +9,8 @@ export class ParseService {
 
   constructor() {
     this.Parse = Parse;
-    this.Parse.initialize('55d6c379-c8c0-4c37-844a-9c33c5484236', 'oNTkB7tj0kBpL1Ebu34AzsEY60jsJG6E');
-    this.Parse.serverURL = 'https://api.parse.buddy.com/parse/';
+    this.Parse.initialize('2erHkvAoNlxz9KS70GpJtogDqu5dntSnH5wpgq76', 'VFstENs9mihDqCmWRCaP8XytcxqzQOojHOR2QZ7n');
+    this.Parse.serverURL = 'https://parseapi.back4app.com';
     this.IsLoggedIn = false;
   }
 
